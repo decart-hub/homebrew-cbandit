2b98394b2cc62c4d2767d1bc3c54570235f284b0
Decart <decart@protonmail.com>
2023-06-15 17:31:40 +0200

    v1.1

--- cbandit.rb
+++ cbandit.rb
@@ -1,4 +1,4 @@
-class ChromeBandit < Formula
+class CBandit < Formula
     desc "Programmatically extract saved passwords from Chromium based browsers on macOS"
     homepage "https://github.com/decart-hub/cbandit"
     url "https://github.com/decart-hub/cbandit/archive/refs/tags/v1.0-beta-0.3.tar.gz"
